4776cd26fed00d82d3eab8ad12b9b49868c72466
Matt Price <105237637+Matt-239@users.noreply.github.com>
2023-12-03 22:59:52 -0500

    update to app.js file for privitization of routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, NavLink, Navigate } from 'react-router-dom';
 import UserList from './components/UserList';
 import UserForm from './components/UserForm';
 import EmployeeList from './components/EmployeeList';
@@ -7,24 +7,32 @@ import EmployeeForm from './components/EmployeeForm';
 import EmployeeDetail from './components/EmployeeDetail';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import { Navigate } from 'react-router-dom';
 
-import './App.css'; 
+import './App.css';
 
 function App() {
   const isAuthenticated = false;
+
+  const PrivateRoute = ({ element, ...props }) => {
+    return isAuthenticated ? (
+      element
+    ) : (
+      <Navigate to="/login" replace state={{ from: props.location }} />
+    );
+  };
 
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/users" exact element={<UserList />} />
-          <Route path="/users/create" element={<UserForm />} />
-          <Route path="/employees" exact element={<EmployeeList />} />
-          <Route path="/employees/create" element={<EmployeeForm />} />
-          <Route path="/employees/:id" element={<EmployeeDetail />} />
+          <Route path="/users" element={<PrivateRoute element={<UserList />} />} />
+          <Route path="/users/create" element={<PrivateRoute element={<UserForm />} />} />
+          <Route path="/employees" element={<PrivateRoute element={<EmployeeList />} />} />
+          <Route path="/employees/create" element={<PrivateRoute element={<EmployeeForm />} />} />
+          <Route path="/employees/:id" element={<PrivateRoute element={<EmployeeDetail />} />} />
+          <Route path="/" element={<Navigate to="/login" />} />
         </Routes>
       </div>
     </Router>
